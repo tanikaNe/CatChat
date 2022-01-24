114b329d5982d0a3e8172f6df676f38c5aeb030f
Weronika <weronikapios7@gmail.com>
2022-01-24 16:32:46 +0100

    login layout and viewPager added

--- app/src/main/java/com/example/catchat/JoinActivity.kt
+++ app/src/main/java/com/example/catchat/JoinActivity.kt
@@ -2,10 +2,39 @@ package com.example.catchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
-class JoinActivity : AppCompatActivity() {
+private const val  NUM_PAGES = 2
+class JoinActivity : FragmentActivity() {
+
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        viewPager = findViewById(R.id.joinViewPager)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+    }
+
+    override fun onBackPressed() {
+        if(viewPager.currentItem ==0) {
+            //if the user is looking at the first step, handle the back button
+            super.onBackPressed()
+        }else {
+            //otherwise select the previous step
+            viewPager.currentItem = viewPager.currentItem -1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+        override fun createFragment(position: Int): Fragment = LoginFragment()
     }
 }
